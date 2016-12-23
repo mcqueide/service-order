a034b3432b269556f63b3b05bd5dbd66b5f2b147
mcqueide <mcqueide@gmail.com>
2016-12-23 18:32:56 -0200

    [UPDATE] Adding new dependencies in pom.xml to unit test

--- service-order/service-order-ejb/src/test/java/br/com/codeshare/data/ClientRepositoryTest.java
+++ service-order/service-order-ejb/src/test/java/br/com/codeshare/data/ClientRepositoryTest.java
@@ -6,10 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -25,40 +22,52 @@ import br.com.codeshare.model.Phone;
 @RunWith(PowerMockRunner.class)
 public class ClientRepositoryTest {
 	
+    private EntityManager em;
+    private EntityManagerFactory emf;
+
 	@InjectMocks
 	private ClientRepository repository;
-	
+
 	@Before
 	public void begin(){
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("mockPU");
-        EntityManager em = emf.createEntityManager();
+        emf = Persistence.createEntityManagerFactory("primary");
+        em = emf.createEntityManager();
         Mockito.when(repository.getEntityManager()).thenReturn(em);
+
+        em.getTransaction().begin();
 	}
 	
+	@After
+    public void end(){
+	    em.close();
+	    emf.close();
+    }
+
 	@Test
 	public void ClientSaveTest(){
-		
+
 		Phone phone = new PhoneBuilder()
 				.withBrand("Samsung")
 				.withModel("Galaxy S6")
 				.buid();
-		
+
 		Client client = new ClientBuilder()
 				.withName("John Mc.Queide")
 				.withAdress("Quadra 101 Conjunto 07 Casa 07")
 				.withHomePhone("(61) 1234-9812")
 				.withPhone(Arrays.asList(phone))
 				.build();
-		
-		repository.getEntityManager().getTransaction().begin();
+
 		repository.insert(client);
 		repository.getEntityManager().getTransaction().commit();
 
 		Client c = repository.findById(client.getId());
-		
+
 		Assert.assertEquals(client.getName(), c.getName());
-		
+
 		repository.getEntityManager().close();
 	}
+
+
 	
 }
